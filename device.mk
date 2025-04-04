6313286064be888a25f3035f2d486c5e4d43f168
Pranav Vashi <neobuddy89@gmail.com>
2025-04-04 12:02:39 +0530

    Spacewar: Build DeviceAsWebcam service
    * Disabled by default on some roms
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off by: Samarth P <smrth097@gmail.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,9 @@ PRODUCT_COPY_FILES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Camera
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
