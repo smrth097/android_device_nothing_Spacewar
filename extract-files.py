e88b42d69f7851fc8cbe7808edd342bc60ef54d4
DaViDev985 <davidev985@gmail.com>
2025-09-03 18:55:06 +0200

    Spacewar: Fix video recording on Nothing Camera This commit adds camera libraries, patches com.vidhance.node.processing.so to make it use libui-v34 and adds com.qti.chi.override.so and camera.qcom.so to public.libraries.txt. Not all of those libraries are probably needed to achieve the fix but they were present in Nothing OS, and were missing here. They only add up to about a megabyte in size, so no real need to debloat them.
    9391  9391 F DEBUG   : *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
    9391  9391 F DEBUG   : DerpFest Version: '16.0'
    9391  9391 F DEBUG   : Build fingerprint: 'Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1666412462:user/release-keys'
    9391  9391 F DEBUG   : Revision: '0'
    9391  9391 F DEBUG   : ABI: 'arm64'
    9391  9391 F DEBUG   : Timestamp: 2025-08-30 16:37:55.970738055+0200
    9391  9391 F DEBUG   : Process uptime: 0s
    9391  9391 F DEBUG   : Cmdline: /vendor/bin/hw/android.hardware.camera.provider@2.4-service_64
    9391  9391 F DEBUG   : pid: 9104, tid: 9377, name: Work_VContext  >>> /vendor/bin/hw/android.hardware.camera.provider@2.4-service_64 <<<
    9391  9391 F DEBUG   : uid: 1047
    9391  9391 F DEBUG   : signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0xffffffffffffffef
    9391  9391 F DEBUG   :     x0  b400007664125a10  x1  0000007451148978  x2  0000000000000030  x3  0000000000000010
    9391  9391 F DEBUG   :     x4  0200007574545f10  x5  0200007574545f00  x6  0000000000000000  x7  0000000000000001
    9391  9391 F DEBUG   :     x8  ffffffffffffffff  x9  0000000000000000  x10 0200007574545ef0  x11 fffffffffffffff0
    9391  9391 F DEBUG   :     x12 0000007769d5af2c  x13 b4000075745452f0  x14 0000000000000000  x15 0000000000000035
    9391  9391 F DEBUG   :     x16 0000007769d500c0  x17 0000007769cc2840  x18 0000007421100000  x19 b4000077040ff3b0
    9391  9391 F DEBUG   :     x20 b400007664125a10  x21 0000007451148978  x22 b4000076b41a1d00  x23 b40000759419c400
    9391  9391 F DEBUG   :     x24 0000007492ce2c98  x25 0000007492cdfd18  x26 0000043800000780  x27 0000000000000001
    9391  9391 F DEBUG   :     x28 0000007451148aa0  x29 0000007451148910
    9391  9391 F DEBUG   :     lr  0000007492c7b6c8  sp  00000074511488f0  pc  0000007492925590  pst 0000000000001000
    9391  9391 F DEBUG   : 8 total frames
    9391  9391 F DEBUG   : backtrace:
    9391  9391 F DEBUG   :       #00 pc 00000000004ee590  /vendor/lib64/libvidhance.so (BuildId: 2aa6dda0d13008621667a77b224d8d5d)
    9391  9391 F DEBUG   :       #01 pc 00000000008446c4  /vendor/lib64/libvidhance.so (BuildId: 2aa6dda0d13008621667a77b224d8d5d)
    9391  9391 F DEBUG   :       #02 pc 00000000003eb32c  /vendor/lib64/libvidhance.so (BuildId: 2aa6dda0d13008621667a77b224d8d5d)
    9391  9391 F DEBUG   :       #03 pc 000000000084d138  /vendor/lib64/libvidhance.so (BuildId: 2aa6dda0d13008621667a77b224d8d5d)
    9391  9391 F DEBUG   :       #04 pc 00000000004003ac  /vendor/lib64/libvidhance.so (BuildId: 2aa6dda0d13008621667a77b224d8d5d)
    9391  9391 F DEBUG   :       #05 pc 00000000004048d8  /vendor/lib64/libvidhance.so (BuildId: 2aa6dda0d13008621667a77b224d8d5d)
    9391  9391 F DEBUG   :       #06 pc 000000000008390c  /apex/com.android.runtime/lib64/bionic/libc.so (__pthread_start(void*)+236) (BuildId: 0c92f4a01e5f29b8bfa168dcfaf027c6)
    9391  9391 F DEBUG   :       #07 pc 0000000000074580  /apex/com.android.runtime/lib64/bionic/libc.so (__start_thread+64) (BuildId: 0c92f4a01e5f29b8bfa168dcfaf027c6)
    
    Signed-off by: Samarth P <smrth097@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libinput_shim.so'),
     ('vendor/lib64/libdpps.so', 'vendor/lib64/libsnapdragoncolor-manager.so',): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/lib64/camera/components/com.vidhance.node.processing.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
